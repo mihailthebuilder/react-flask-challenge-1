1e37ece8b6357f3eb86d1df05358a71cacaa23ef
Mihail Marian <mihailthebuilder@gmail.com>
2021-05-20 20:49:13 +0100

    linter pass

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ RD_MAP = {"M": 1000, "D": 500, "C": 100, "L": 50, "X": 10, "V": 5, "I": 1}
 def roman_to_decimal(roman):
 
     decimal = 0
-    
+
     roman_len = len(roman)
     invalid_str_message = "Please enter a valid Roman numeral string"
 
@@ -25,9 +25,6 @@ def roman_to_decimal(roman):
             char_2_value = RD_MAP[roman[i + 1]]
         except KeyError:
             return invalid_str_message
-
-        if char_2_value > char_1_value and char_2_value //
-
 
 
 if __name__ == "__main__":
